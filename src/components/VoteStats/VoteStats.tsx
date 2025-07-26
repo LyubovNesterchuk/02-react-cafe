607636203fb3ee394db00142127832f8956aebc3
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-07-26 19:34:58 +0300

    update

--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,5 +1,5 @@
 import type { Votes } from '../../types/votes';
-import styles from './VoteStats.module.css';
+import css from './VoteStats.module.css';
 
 interface VoteStatsProps {
   votes: Votes;
@@ -9,12 +9,12 @@ interface VoteStatsProps {
 
 export default function VoteStats({ votes, totalVotes, positiveRate }: VoteStatsProps) {
   return (
-    <div className={styles.container}>
-      <p className={styles.stat}>Good: <strong>{votes.good}</strong></p>
-      <p className={styles.stat}>Neutral: <strong>{votes.neutral}</strong></p>
-      <p className={styles.stat}>Bad: <strong>{votes.bad}</strong></p>
-      <p className={styles.stat}>Total: <strong>{totalVotes}</strong></p>
-      <p className={styles.stat}>Positive: <strong>{positiveRate}%</strong></p>
+    <div className={css.container}>
+      <p className={css.stat}>Good: <strong>{votes.good}</strong></p>
+      <p className={css.stat}>Neutral: <strong>{votes.neutral}</strong></p>
+      <p className={css.stat}>Bad: <strong>{votes.bad}</strong></p>
+      <p className={css.stat}>Total: <strong>{totalVotes}</strong></p>
+      <p className={css.stat}>Positive: <strong>{positiveRate}%</strong></p>
     </div>
   );
 }
